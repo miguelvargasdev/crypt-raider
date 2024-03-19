58935e3df327b855c308caf19bdc3a7ee8e3fa2d
Miguel Vargas <contact@miguelvargas.dev>
2024-03-19 16:33:42 -0400

    Refactor bag container and add item placement logic

--- inventory/bag/bag_container.gd
+++ inventory/bag/bag_container.gd
@@ -1,43 +1,29 @@
+class_name BagContainer
 extends MarginContainer
 
-# FILEPATH: /home/moogle/Documents/Projects/crypt-raider/inventory/bag/bag_container.gd
-
-# The scene to be used as a template for each slot in the bag container.
-@export var _slot: PackedScene
-
-# The GridContainer node that will hold the slots in the bag container.
-@onready var grid_container: GridContainer = $GridContainer
-
-# The Bag instance associated with this bag container.
 var _bag: Bag
-
-# An array to store references to the slots in the bag container.
 var slots: Array
 
-# The inventory of items in the bag.
+@export var _slot: PackedScene
 @export var _bag_inventory: Array[Item]
 
-# The container node that will hold the item instances in the bag container.
 @onready var _item_container
+@onready var grid_container: GridContainer = $GridContainer
 
-# Sets the bag for the bag container.
-# This function assigns the provided `bag` to the `_bag` variable and calls `_instantiate_grid()` to instantiate the grid for the bag.
 func set_bag(bag: Bag):
 	_bag = bag
 	_instantiate_grid()
 
-# Instantiates the grid based on the bag size.
 func _instantiate_grid():
 	if _bag == null: return
 
 	grid_container.columns = _bag.size.x
 	_generate_grid(_bag.size.x, _bag.size.y)
 		
-# Generates a grid of slots in the bag container.
-# Each slot is instantiated, positioned, and connected to the slot_clicked signal.
-# The slots are added as children to the grid_container node.
-# The slots are stored in a 2D array for easy access.
 func _generate_grid(col: int, row: int):
+	if _slot == null:
+		printerr("Error: _slot is null. Please assign a PackedScene to it.")
+		return
 	for i in range(row):
 		var nested_arr = []
 		for j in range(col):
@@ -48,19 +34,15 @@ func _generate_grid(col: int, row: int):
 			nested_arr.push_back(new_slot) 
 		slots.push_back(nested_arr)
 
-# Called when a slot is clicked in the bag container.
-# Checks if the item can fit in the bag from the clicked slot.
 func _on_slot_clicked(slot: Slot):
 	print_rich("Slot clicked: [color=green]%s (%s,%s)" % [_bag.bag_name, slot.pos.x, slot.pos.y])
 	_can_item_fit_in_bag_from_slot(_bag_inventory[0], _bag, slot)
 
-# Checks if an item can fit in a bag from a specific slot.
-# Returns true if the item can fit, false otherwise.
 func _can_item_fit_in_bag_from_slot(item: Item, bag: Bag, slot: Slot) -> bool:
-	var slot_pos = Vector2(slot._x_position, slot._y_position)
+	var slot_pos = Vector2(slot.pos.x, slot.pos.y)
 	
 	if bag.size.x - slot_pos.x >= item.size.x && bag.size.y - slot_pos.y >= item.size.y:
-		if _are_slots_occupied(item, bag, slot):
+		if _are_slots_occupied(item, slot):
 			print("%s can fit here." % item.name)
 			return true
 		else:
@@ -70,17 +52,30 @@ func _can_item_fit_in_bag_from_slot(item: Item, bag: Bag, slot: Slot) -> bool:
 	print("%s cannot fit here." % item.name)
 	return false
 
-# Checks if the slots specified by the given item, bag, and slot are occupied.
-# Returns true if all the slots are unoccupied, otherwise returns false.
-func _are_slots_occupied(item: Item, bag: Bag, slot: Slot) -> bool:
-	var slot_pos = Vector2(slot.pos.x,slot.pos.y)
-	for i in range(slot_pos.x, slot_pos.x + item.size.x):
-		for j in range(slot_pos.y, slot_pos.y + item.size.y):
-			if i >= slots.size() or j >= slots[i].size():
-				return false
+func _are_slots_occupied(item: Item, slot: Slot) -> bool:
+	var slot_pos = Vector2(slot.pos.x, slot.pos.y)
+	var end_x = slot_pos.x + item.size.x
+	var end_y = slot_pos.y + item.size.y
+
+	if end_x > slots.size() or end_y > slots[slot_pos.x].size():
+		return false
+
+	for i in range(slot_pos.y, end_y):
+		for j in range(slot_pos.x, end_x):
+			print("Position: %s, Occupied? %s, (i,j): (%s,%s)" % [slots[i][j].pos, slots[i][j]._occupied,i, j])
 			if slots[i][j]._occupied:
 				return false
+
+	_place_item(item, slot_pos.x, slot_pos.y, end_x, end_y)
 	return true
+
+func _place_item(item: Item, start_x: int, start_y: int, end_x: int, end_y: int):
+	for i in range(start_y, end_y):
+		for j in range(start_x, end_x):
+			slots[i][j].set_item(item)
+
+	slots[start_y][start_x].set_item_container(item)
+	
 
 func _ready():
 	_item_container = preload("res://inventory/item/item_container.tscn")
