5394ab4fb6278e05c294069037063b2d3e2ac7d4
Miguel Vargas <contact@miguelvargas.dev>
2024-03-19 16:34:06 -0400

    Refactor slot.gd and slot.tscn files

--- inventory/slot/slot.gd
+++ inventory/slot/slot.gd
@@ -2,13 +2,12 @@ class_name Slot
 extends Control
 
 var _occupied = false
-var pos: Vector2i
-var _x_position: int = 0
-var _y_position: int = 0
+var pos: Vector2i = Vector2i(0,0)
 var _item: Item
 @onready var xy_label: Label
 @onready var item_label: Label
 @onready var item_texture: TextureRect
+@onready var item_container: PanelContainer
 
 signal slot_clicked(slot: Slot)
 
@@ -19,17 +18,20 @@ func _gui_input(event):
 func _ready():
 	xy_label = get_node("XYPosition")
 	item_label = $ItemLabel
-	item_texture =$ItemTexure
-	xy_label.text = "(%s,%s)" % [pos.x, pos.y] 
+	item_texture =$ItemContainer/ItemIcon
+	item_container = $ItemContainer
+	xy_label.text = "(%s,%s)" % [pos.x, pos.y]
 
 func set_slot_position(x: int, y: int):
 	pos.x = x
 	pos.y = y
 
 func set_item(item: Item):
-	
 	_item = item
 	item_label.text = item.name
 	item_texture.texture = item.icon as Texture2D
 	_occupied = true
 
+func set_item_container(item: Item):
+	item_container.set_size(item.size * 32)
+	item_container.show()
